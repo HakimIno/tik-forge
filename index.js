f88db8b2c0d8caafdc747e7b56350946a630a5b5
hakimino_o <weerachit.utnok01@gmail.com>
2024-12-02 10:12:54 +0700

    test: add comprehensive integration tests

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const tik_forge = require('./build/Release/tik-forge.node');
-module.exports = tik_forge;
+const path = require('path');
+const modulePath = path.join(__dirname, 'build', 'Release', 'tik-forge.node');
+const addon = require(modulePath);
+
+module.exports = {
+    init: addon.init,
+    generatePDF: addon.generatePDF,
+};
